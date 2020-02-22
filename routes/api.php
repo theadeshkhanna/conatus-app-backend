293fb54aefd62a382173e7ecc2ce6e92d67266c8
Adesh Khanna <adeshkhanna271@gmail.com>
2020-02-22 14:11:19 +0530

    [added] test route

--- routes/api.php
+++ routes/api.php
@@ -6,4 +6,7 @@ $basePath     = "App\Api\V1\Controllers\\";
 
 $api->version('v1', function ($api) use ($basePath){
     $api->post('register', $basePath . 'RegistrationController@create');
+    $api->get('/', function () {
+       return 'working';
+    });
 });
